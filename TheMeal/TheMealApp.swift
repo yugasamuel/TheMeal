cd4ee6c59c1d06e0a87417c613fcbcc3d6f9cace
Yuga Samuel <yugasamuel2002@gmail.com>
2023-08-14 16:16:20 +0700

    Initial commit

---
+++ TheMeal/TheMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMealApp.swift
+//  TheMeal
+//
+//  Created by Yuga Samuel on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct TheMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
